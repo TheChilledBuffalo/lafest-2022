4ea2fd7512f13799cc2350149b0aaa41648bd245
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2024-07-14 21:25:43 +0530

    feat: add Russo_One font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,3 +3,7 @@ import localFont from 'next/font/local'
 export const Elianto = localFont({
     src: '../fonts/Elianto-Regular.ttf',
 })
+
+export const RussoOne = localFont({
+    src: '../fonts/Russo-Regular.ttf',
+})
